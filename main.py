0d1e0a18c511719daa9cf3d1663f2748ff3f74e3
Armando Herrera <mail@jellowfish.com>
2020-05-14 21:30:42 -0400

    wrong precedence

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 import readline
 
+from lark import Lark, Transformer
 
-def read(text):
-    # remove comments and split tokens
-    text = text.split('#', 1)[0].split()
-    print(text)
+
+class Exys(Transformer):
+    def atom(self, atom):
+        (atom,) = atom
+        return str(atom)
+
+    def expr(self, expr):
+        (expr,) = expr
+        return expr
+
+    def expr_not(self, expr):
+        (expr,) = expr
+        return ('not', expr)
+
+    def expr_impl(self, expr):
+        (lhs,rhs,) = expr
+        print(expr)
+        return ('impl', lhs, rhs)
+
+
+
+exys = Lark(r"""
+
+%import common.WS
+%ignore WS
+
+expr_head: expr_not -> expr
+
+expr_not: expr_impl -> expr
+    | ( "!" expr_impl ) -> expr_not
+
+expr_impl: expr_last -> expr
+    | ( expr_impl "=>" expr_last ) -> expr_impl
+
+expr_last: atom -> expr
+    | ( "(" expr_head ")" ) -> expr
+
+
+atom: /[a-zA-Z]+/
+
+""", start='expr_head', parser='lalr', transformer=Exys())
 
 
 
@@ -12,8 +50,11 @@ def repl(prompt):
     try:
         while True:
             text = input(prompt)
-            tree = read(text)
-            print(tree)
+            try:
+                tree = exys.parse(text)
+                print(tree)
+            except Exception as e:
+                print(f"{type(e)}:", e)
     except KeyboardInterrupt:
         print()
 
