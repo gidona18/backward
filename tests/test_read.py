063c615ec17944c022f6dd67f26d93a6f8e63937
Armando Herrera <mail@jellowfish.com>
2020-05-18 15:31:45 -0400

    test and

--- tests/test_read.py
+++ tests/test_read.py
@@ -6,12 +6,33 @@ from backward import Backward
 
 
 class TestRead(unittest.TestCase):
+    def test_empty(self):
+        ctx = Backward()
+        ans = ctx.read("")
+        self.assertEqual(str(ans), "[]")
+
     def test_atom(self):
         ctx = Backward()
         ans = ctx.read("a")
         self.assertEqual(str(ans), "[atom(a)]")
         ans = ctx.read("a b c")
         self.assertEqual(str(ans), "[atom(a), atom(b), atom(c)]")
+    
+    def test_not(self):
+        ctx = Backward()
+        ans = ctx.read("!a")
+        self.assertEqual(str(ans), "[not(atom(a))]")
+        ans = ctx.read("!a !b")
+        self.assertEqual(str(ans), "[not(atom(a)), not(atom(b))]")
+    
+    def test_and(self):
+        ctx = Backward()
+        ans = ctx.read("a & b")
+        self.assertEqual(str(ans), "[and((atom(a), atom(b)))]")
+        ans = ctx.read("!a & b")
+        self.assertEqual(str(ans), "[and((not(atom(a)), atom(b)))]")
+        ans = ctx.read("a & !b")
+        self.assertEqual(str(ans), "[and((atom(a), not(atom(b))))]")
 
 if __name__ == '__main__':
     unittest.main()
