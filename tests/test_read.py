32ae4ffff7d771b85554e512966b005f4a165872
Armando Herrera <mail@jellowfish.com>
2020-05-18 15:36:30 -0400

    test

--- tests/test_read.py
+++ tests/test_read.py
@@ -33,6 +33,30 @@ class TestRead(unittest.TestCase):
         self.assertEqual(str(ans), "[and((not(atom(a)), atom(b)))]")
         ans = ctx.read("a & !b")
         self.assertEqual(str(ans), "[and((atom(a), not(atom(b))))]")
+        ans = ctx.read("a & b & c")
+        self.assertEqual(str(ans), "[and((and((atom(a), atom(b))), atom(c)))]")
+    
+    def test_or(self):
+        ctx = Backward()
+        ans = ctx.read("a | b")
+        self.assertEqual(str(ans), "[or((atom(a), atom(b)))]")
+        ans = ctx.read("!a | b")
+        self.assertEqual(str(ans), "[or((not(atom(a)), atom(b)))]")
+        ans = ctx.read("a | !b")
+        self.assertEqual(str(ans), "[or((atom(a), not(atom(b))))]")
+        ans = ctx.read("a | b & c")
+        self.assertEqual(str(ans), "[and((or((atom(a), atom(b))), atom(c)))]")
+
+    def test_xor(self):
+        ctx = Backward()
+        ans = ctx.read("a ^ b")
+        self.assertEqual(str(ans), "[xor((atom(a), atom(b)))]")
+        ans = ctx.read("!a ^ b")
+        self.assertEqual(str(ans), "[xor((not(atom(a)), atom(b)))]")
+        ans = ctx.read("a ^ !b")
+        self.assertEqual(str(ans), "[xor((atom(a), not(atom(b))))]")
+        ans = ctx.read("a ^ b | c")
+        self.assertEqual(str(ans), "[or((xor((atom(a), atom(b))), atom(c)))]")
 
 if __name__ == '__main__':
     unittest.main()
