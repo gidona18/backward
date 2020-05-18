902136bcf719a42beb78c7abc1913d9ff03dfe8d
Armando Herrera <mail@jellowfish.com>
2020-05-18 14:54:17 -0400

    repr wip

--- backward/backward.py
+++ backward/backward.py
@@ -1,46 +1,4 @@
-from lark import Lark, Transformer
-
-
-# ----------------------------------------------------------------------
-
-
-class XSysTransformer(Transformer):
-    pass
-
-xsys_grammar = Lark(r"""
-%import common.WS
-%ignore WS
-
-stmt_expr: stmt_head -> stmt
-    | expr_head -> expr
-
-stmt_head: ( expr_head "=>" expr_head ) -> stmt_impl
-    | ( "=" atom* ) -> stmt_init
-    | ( "?" atom* ) -> stmt_look
-
-expr_head: expr_and -> expr
-
-expr_and: expr_or -> expr
-    | ( expr_and "&" expr_or ) -> expr_and
-
-expr_or: expr_xor -> expr
-    | ( expr_or "|" expr_xor ) -> expr_or
-
-expr_xor: expr_last -> expr
-    | ( expr_xor "^" expr_last ) -> expr_xor
-
-expr_last: atom -> expr
-    | ( "!" atom ) -> expr_not
-    | ( "!" "(" expr_head ")" ) -> expr_not
-    | ( "(" expr_head ")" ) -> expr
-
-atom: /[a-zA-Z]+/
-
-""",
-    start="stmt_expr",
-    parser="lalr",
-    transformer=XSysTransformer(),
-)
+from .read import read
 
 
 class Interpreter(): 
@@ -48,5 +6,5 @@ class Interpreter():
         self.context = {}
     
     def interpret(self, text):
-        tree = xsys_grammar.parse(text)
+        tree = read(text)
         return tree
--- backward/read.py
+++ backward/read.py
@@ -1,9 +1,28 @@
 from lark import Lark, Transformer
 
-class XSys(Transformer):
-    pass
+from protoclass import proto
 
-xsys = Lark(r"""
+
+class XSys(Transformer):
+    def start(self, args):
+        return args[:]
+
+    def stmt(self, stmt):
+        (stmt,) = stmt
+        return stmt
+    
+    def expr(self, expr):
+        (expr,) = expr
+        return expr
+
+
+    def atom(self, atom):
+        atom = str(atom[0])
+        prot = proto(kind='atom', data=atom)
+        prot.__repr__ = lambda self: f"{self.kind}({self.data})"
+        return prot
+
+XSYS_GRAMMAR = Lark(r"""
 
     %import common.WS
     %ignore WS
@@ -36,3 +55,7 @@ xsys = Lark(r"""
         | ( "(" expr_head ")" )
 
 """, parser="lalr", transformer=XSys())
+
+
+def read(text):
+    return XSYS_GRAMMAR.parse(text)
